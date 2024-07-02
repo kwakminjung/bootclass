521fce681334ceb264953dd5068ea26e935480da
KwakMJ <kwak2002mj@gmail.com>
2024-07-02 16:28:56 +0900

    first commit

---
+++ Day2/pororo/src/main/java/com/example/pororo/PororoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pororo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PororoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PororoApplication.class, args);
+	}
+
+}
---
+++ Day2/pororo/src/test/java/com/example/pororo/PororoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pororo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PororoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
